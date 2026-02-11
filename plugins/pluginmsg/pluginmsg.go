978591a6d6f585587fab2ae6a7bf9b399f7a0ed4
NilssonCreative <anders@nilssoncreative.se>
2026-02-11 17:45:49 +0100

    fix: improve logging and event subscription in PluginMsg

--- plugins/pluginmsg/pluginmsg.go
+++ plugins/pluginmsg/pluginmsg.go
@@ -2,7 +2,6 @@ package pluginmsg
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/go-logr/logr"
 	"github.com/robinbraemer/event"
@@ -13,7 +12,10 @@ import (
 var Plugin = proxy.Plugin{
 	Name: "PluginMsg",
 	Init: func(ctx context.Context, p *proxy.Proxy) error {
-		log := logr.FromContextOrDiscard(ctx).WithName("PluginMsg")
+		log, err := logr.FromContext(ctx)
+		if err != nil {
+			return err
+		}
 
 		return newPluginMsg(p, &log).init()
 	},
@@ -35,20 +37,14 @@ func newPluginMsg(proxy *proxy.Proxy, log *logr.Logger) *PluginMsg {
 func (p *PluginMsg) init() error {
 	//p.registerCommands()
 	p.registerSubscribers()
-	p.registerPluginChannels()
+	//p.registerPluginChannels()
 	return nil
-}
-
-func (p *PluginMsg) registerPluginChannels() {
-	// Register a plugin channel for sending messages to the client.
-	//p.Proxy().RegisterPluginChannel("my:channel")
-	p.ChannelRegistrar().Register(message.LegacyChannelIdentifier("luckperms:update"))
 }
 
 // Register event subscribers
 func (p *PluginMsg) registerSubscribers() {
 	// Send message on server switch.
-	//event.Subscribe(p.Event(), 0, p.onServerSwitch)
+	event.Subscribe(p.Event(), 0, p.onServerSwitch)
 
 	// Change the MOTD response.
 	//event.Subscribe(p.Event(), 0, pingHandler())
@@ -58,9 +54,37 @@ func (p *PluginMsg) registerSubscribers() {
 
 	// Listen for plugin messages.
 	event.Subscribe(p.Event(), 0, p.onPluginMessage)
+
+	// Listen for plugin messages during login.
+	event.Subscribe(p.Event(), 0, p.onServerLoginPluginMessage)
+
+	p.log.Info("Registered plugin message event subscriber")
+}
+
+func (p *PluginMsg) registerPluginChannels() {
+	// Register a plugin channel for sending messages to the client.
+	//p.Proxy().RegisterPluginChannel("my:channel")
+
+	luckId, err := message.NewChannelIdentifier("pluginmsg", "main")
+	if err != nil {
+		p.log.Error(err, "Failed to create plugin channel identifier")
+		return
+	}
+
+	p.ChannelRegistrar().Register(luckId)
+}
+
+func (p *PluginMsg) onServerLoginPluginMessage(e proxy.ServerLoginPluginMessageEvent) {
+	p.log.Info("ServerLoginPluginMessageEvent fired!")
+
+	res := e.Result()
+
+	p.log.Info("Plugin message received during login", "allowed", res.Allowed())
 }
 
 func (p *PluginMsg) onPluginMessage(e proxy.PluginMessageEvent) {
+
+	p.log.Info("PluginMessageEvent fired!")
 
 	// Another plugin may have already cancelled the event.
 	if !e.Allowed() {
@@ -68,18 +92,24 @@ func (p *PluginMsg) onPluginMessage(e proxy.PluginMessageEvent) {
 		return
 	}
 
-	if _, ok := e.Target().(proxy.Player); ok {
-		// e.Source() IS a proxy.Player
-		// 'player' is now the concrete value
-		p.log.Info("Plugin message receieved with target player, do nothing")
-		return
-	}
+	// if _, ok := e.Target().(proxy.Player); ok {
+	// 	// e.Source() IS a proxy.Player
+	// 	// 'player' is now the concrete value
+	// 	p.log.Info("Plugin message receieved with target player, do nothing")
+	// 	return
+	// }
 
-	e.SetForward(false)
+	// e.SetForward(false)
 
-	if e.Source() == nil {
-		p.log.Info("Plugin message received", "source type", "<nil>", "length", len(e.Data()))
-	} else {
-		p.log.Info("Plugin message received", "source type", fmt.Sprintf("%T", e.Source()), "length", len(e.Data()))
-	}
+	// if e.Source() == nil {
+	// 	p.log.Info("Plugin message received", "source type", "<nil>", "length", len(e.Data()))
+	// } else {
+	// 	p.log.Info("Plugin message received", "source type", fmt.Sprintf("%T", e.Source()), "length", len(e.Data()))
+	// }
+}
+
+func (p *PluginMsg) onServerPreConnect(e proxy.ServerPreConnectEvent) {
+	p.log.Info("ServerPreConnectEvent fired!")
+
+	//p.registerPluginChannels()
 }
